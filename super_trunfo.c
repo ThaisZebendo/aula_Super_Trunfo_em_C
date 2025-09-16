d72f5a3143ef55261aa546774658237ac72eb574
ThaisZebendo <zebendothais@gmail.com>
2025-09-16 13:39:14 -0300

    Update super_trunfo.c
    Super trunfo em C: nível aventureiro

--- super_trunfo.c
+++ super_trunfo.c
@@ -10,6 +10,8 @@ int main (){
     int pontosturisticos1;
     float area1;
     float pib1;
+    float densidade1 = populacao1/area1;
+    float pibpercapita1 = pib1/populacao1;
 
 //Informações Carta 2
     char estado2; 
@@ -19,6 +21,8 @@ int main (){
     int pontosturisticos2;
     float area2;
     float pib2;
+    float densidade2 = populacao2/area2;
+    float pibpercapita2 = pib2/populacao2;
 
 //Cadastro de Carta 1
     printf("Cadastro de Carta 1\n");
@@ -67,7 +71,7 @@ int main (){
 
     printf("Digite o número de pontos turísticos: \n");
     scanf("%d", &pontosturisticos2);
-
+    
 //Exibição das Cartas
     printf("Carta 1\n");
     printf("Estado: %c\n", estado1);
@@ -77,6 +81,8 @@ int main (){
     printf("Área: %.2f KM²\n", area1);
     printf("PIB: %.2f bilhões de reais\n", pib1);
     printf("Pontos Turísticos: %d\n", pontosturisticos1);
+    printf("Densidade populacional: %.2f\n", densidade1);
+    printf("PIB per capita: &.2f\n", pibpercapita1);
 
     printf("Carta 2\n");
     printf("Estado:  %c\n", estado2);
@@ -86,6 +92,8 @@ int main (){
     printf("Área: %.2f KM²\n", area2);
     printf("PIB: %.2f bilhões de reais\n", pib2);
     printf("Pontos Turísticos: %d\n", pontosturisticos2);
+    printf("Densidade populacional: %.2f\n", densidade2);
+    printf("PIB per capita: &.2f\n", pibpercapita2);
 
   return 0;
 }
